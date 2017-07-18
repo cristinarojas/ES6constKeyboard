f89709b34916bc4f61f58f5bac2611f21e73b023
Cristina Rojas Zamora <criss@Cristinas-MacBook-Pro-2.local>
2017-07-18 15:43:11 -0700

    Adding excercise of const variable

---
+++ const.js
@@ -0,0 +1,9 @@
+// The const keyword is used to declare the read-only variables
+// provide a native protection to the constant variables.
+
+const pi = 3.1416;
+var r = 2;
+
+console.log(pi * r * r);
+
+//pi = 12; // Throws read-only exception
